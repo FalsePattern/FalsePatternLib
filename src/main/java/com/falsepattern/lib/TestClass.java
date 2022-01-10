0b07be04e1aa776b8d4781b4c0f1ff99552bf543
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-01-10 15:51:43 +0100

    drop unneeded test file

--- src/main/java/com/falsepattern/lib/TestClass.java
+++
@@ -1,9 +0,0 @@
-package com.falsepattern.lib;
-
-import com.falsepattern.json.node.JsonNode;
-
-public class TestClass {
-    public static void test() {
-        FalsePatternLib.libLog.info(JsonNode.parse("{\"test\":\"hi\"}").get("test").stringValue());
-    }
-}
