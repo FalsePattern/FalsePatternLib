b2dd08c4c50f620d010239da731398c2a2bb80c7
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-05-06 13:27:22 +0200

    better wording

--- src/main/java/com/falsepattern/lib/api/Deprecation.java
+++ src/main/java/com/falsepattern/lib/api/Deprecation.java
@@ -3,8 +3,8 @@ package com.falsepattern.lib.api;
 final class Deprecation {
     static void warn() {
         new Exception("\nDEPRECATION WARNING\nDEPRECATION WARNING\n" +
-                      "Someone used the deprecated FalsePatternLib api.\n" +
-                      "This api will be replaced in FalsePatternLib 0.7! You should probably update your mods.\n" +
-                      "See the following stacktrace for hints on what mod could be the cause.").printStackTrace();
+                      "Someone used the deprecated FalsePatternLib api from 0.5 and before.\n" +
+                      "This api was deprecated in 0.6.0, and will be REMOVED in FalsePatternLib 0.7! You should probably update your mods.\n" +
+                      "See the following stacktrace for hints on what mod might be the cause.").printStackTrace();
     }
 }
