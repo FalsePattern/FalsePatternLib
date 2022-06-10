5cc487f9a9f949dd586ab16811db3de37171a486
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-06-10 03:00:23 +0200

    add async library documentation

--- src/main/java/com/falsepattern/lib/util/Async.java
+++ src/main/java/com/falsepattern/lib/util/Async.java
@@ -7,6 +7,11 @@ import lombok.Getter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * Asynchronous utilities. Notice: you should not abuse this for anything performance-sensitive.
+ * The asynchronous worker exists primarily to run code independently of the main thread during loading.
+ * You must make sure to solve any potential deadlocks yourself!
+ */
 @StableAPI(since = "0.8.0")
 public class Async {
     public static final ExecutorService asyncWorker = Executors.newSingleThreadExecutor((runnable) -> {
