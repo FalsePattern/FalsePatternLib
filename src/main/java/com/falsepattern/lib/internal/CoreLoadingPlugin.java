7f1e4fe2c312c776983b6d896b7432de31203a2a
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-07-25 16:44:00 +0200

    disallow using Thermos with my mods, it only causes unnecessary headaches.

--- src/main/java/com/falsepattern/lib/internal/CoreLoadingPlugin.java
+++ src/main/java/com/falsepattern/lib/internal/CoreLoadingPlugin.java
@@ -1,5 +1,7 @@
 package com.falsepattern.lib.internal;
 
+import lombok.val;
+
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
@@ -13,6 +15,19 @@ import java.util.Map;
 @Name(Tags.MODID)
 @SortingIndex(500)
 public class CoreLoadingPlugin implements IFMLLoadingPlugin {
+    static {
+        try {
+            Class.forName("thermos.Thermos");
+            val iAmNotFixingThisDontEvenAskPlusSkillIssue =
+                    new Error("\n\n" +
+                              "--------------------------------------------------------------------------------\n" +
+                              "|Thermos is not supported by FalsePatternLib, please use a normal forge server.|\n" +
+                              "|       Any bug reports concerning this message will be silently deleted.      |\n" +
+                              "--------------------------------------------------------------------------------\n");
+            iAmNotFixingThisDontEvenAskPlusSkillIssue.setStackTrace(new StackTraceElement[0]);
+            throw iAmNotFixingThisDontEvenAskPlusSkillIssue;
+        } catch (ClassNotFoundException ignored) {}
+    }
 
     @Override
     public String[] getASMTransformerClass() {
